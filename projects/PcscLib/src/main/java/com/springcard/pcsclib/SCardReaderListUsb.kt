9fa38a3cc62ffc7d4f745732a4779c591c90e32e
Corentin Raoult <corentin.r@springcard.com>
2019-02-13 15:58:48 +0100

    [springcard.pcsc.android] add license

--- projects/PcscLib/src/main/java/com/springcard/pcsclib/SCardReaderListUsb.kt
+++ projects/PcscLib/src/main/java/com/springcard/pcsclib/SCardReaderListUsb.kt
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2018-2019 SpringCard - www.springcard.com
+ * All right reserved
+ * This software is covered by the SpringCard SDK License Agreement - see LICENSE.txt
+ */
+
 package com.springcard.pcsclib
 
 import android.hardware.usb.UsbDevice
